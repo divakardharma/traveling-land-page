07d9c4478f29646a28850d8cb8cd7b4983d88894
divakardharma2003 <divakardharma2003@gmail.com>
2024-08-31 23:57:04 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./app.css"
+import "./App.css"
 import Home from "./components/Home/home"
 import Main from "./components/Main/main"
 import Navbar from './components/Navbar/navbar'
